629aaf90ba50b2d7950745be1fe0dbac80296666
tanoshiibot <fulljelly@outlook.fr>
2020-11-04 10:28:59 +0100

    movieCase()

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
 $('#moviePLaying1').modal('show')
+
+function movieCase(poster, name, year, genre) {
+    let film = document.createElement("P")
+    [poster, name, year, genre].forEach(x => {
+        x == poster ? x = document.createElement("IMG").setAttribute("src", poster) : x = document.createElement("P").setAttribute("value", x);
+        film.appendChild(x)
+    })
+}
