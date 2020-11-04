d71563fb9539c2b04877f23f7b84c5e6be2c0ce7
tanoshiibot <fulljelly@outlook.fr>
2020-11-04 13:25:47 +0100

    exampleCards, et un fetch de 5 films de ghibli

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,26 +9,31 @@
 </div>
 </div>
 */
-let film;
+
 
 function movieCase(poster, name, year, genre) {
+    let film;
     film = `<div class="card" style="width: 11rem" data-toggle="modal" data-target="#moviePLayingCard">`;
-    let filmData = [poster, name, year, genre];
-    filmData.forEach((x, i) => {
-        let a;
-        switch (x) {
-            
-        }
-        x == poster ? a = `<img class="card-img-top" src="https://image.tmdb.org/t/p/w500${x}" alt="CardImageCap">` : a = `<p>${x}</p>`;
-        film.innerHTML += a;
-    })
-    film += `</div>`
-    document.getElementById("test1").innerHTML = film;
+    film += `<img class="card-img-top" src="https://image.tmdb.org/t/p/w500${poster}" alt="CardImageCap">` 
+    film += `<h3 class="card-title">${name}</h3>`;
+    film += `<div class="d-flex justify-content-between"><p class="card-text m-0">${year}</p>`
+    film += `<p class="m-0">${genre}</p>`
+    film += `</div></div>`
+    return film;
 }
 
-fetch('https://api.themoviedb.org/3/movie/550?api_key=a05fba96f4d3bad807d07845d4896afb').then(response => response.json()).then(data => {
-    console.log("yay");
-    //document.getElementById("test1").innerHTML = 
-    movieCase(data.poster_path, data.title, data.release_date, data.genres[0].name)
-    console.log(film)
+let exampleCards = [];
+fetch('https://api.themoviedb.org/3/discover/movie?api_key=a05fba96f4d3bad807d07845d4896afb&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_crew=608&with_companies=10342').then(response => response.json()).then(data => {
+    for (let i = 0; i < 5; i++) {
+        exampleCards.push({
+            "id" : data.results[i].id,
+            "poster" : data.results[i].poster_path,
+            "name" : data.results[i].title,
+            "year" : data.results[i].release_date.substring(0, 4),
+            "genre_ids" : data.results[i].genre_ids
+        })
+    }
+    console.log(exampleCards);
 });
+
+
