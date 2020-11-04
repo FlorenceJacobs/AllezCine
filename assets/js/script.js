af9f2adc74e9608fb59e9442b33a8240c97246e4
tanoshiibot <fulljelly@outlook.fr>
2020-11-04 20:30:22 +0100

    boutons more et less

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,7 @@ function movieCase(poster, name, year, genre) {
     let film;
     film = `<div class="card moviePlayingCard" style="width: 11rem" data-toggle="modal" data-target="#moviePLayingCard">`;
     film += `<img class="card-img-top" src="https://image.tmdb.org/t/p/w500${poster}" alt="CardImageCap">` 
-    film += `<h3 class="card-title">${name}</h3>`;
+    film += `<h3 class="card-title h5">${name}</h3>`;
     film += `<div class="d-flex justify-content-between"><p class="card-text m-0">${year}</p>`
     film += `<p class="m-0">${genre}</p>`
     film += `</div></div>`
@@ -35,16 +35,44 @@ fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=a05fba96f4d3bad807d
 function genreName(object, id) {
     genreList.forEach(x => {
         if (id == x.id) {
-            object.genre_name = x.name
+            object.genre_name = x.name;
         }
     })
 }
 
-
+let currentFeatured = [];
+function featuredFilms(x, inc) {
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=a05fba96f4d3bad807d07845d4896afb&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1${x == 0 ? "" : `&with_genres=${x}`}`).then(response => response.json()).then(data => {
+        console.log(x, inc);
+        for (let i = (inc * 6); i < (inc * 6 + 6); i++) {
+            if (data.results[i] != undefined) {
+                currentFeatured.push({
+                    "id" : data.results[i].id,
+                    "poster" : data.results[i].poster_path,
+                    "name" : data.results[i].title,
+                    "year" : data.results[i].release_date.substring(0, 4),
+                    "genre_ids" : data.results[i].genre_ids
+                })
+            } else {
+                document.getElementById("more").setAttribute("class", "btn d-none");
+            }
+        };
+        
+        currentFeatured.forEach((element, i) => {
+            if (i > inc * 6) {
+                genreName(element, element.genre_ids[0]);
+                document.getElementById("featuredList").innerHTML += `<li>${movieCase(element.poster, element.name, element.year, element.genre_name)}</li>`;
+            }
+        });
+    }); 
+}
+    
+    
 let exampleCards = [];
 //afficher exampleCards
 fetch('https://api.themoviedb.org/3/discover/movie?api_key=a05fba96f4d3bad807d07845d4896afb&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_crew=608&with_companies=10342').then(response => response.json()).then(data => {
     for (let i = 0; i < 5; i++) {
+
         exampleCards.push({
             "id" : data.results[i].id,
             "poster" : data.results[i].poster_path,
@@ -62,28 +90,37 @@ fetch('https://api.themoviedb.org/3/discover/movie?api_key=a05fba96f4d3bad807d07
 });
 
 let featuresButtons = [0, 28, 35, 18, 99, 27, 10751, 80, 14]
-console.log(featuresButtons);
+let incFilms = 0;
+let currentGenre = 0;
 
-currentFeatured = [];
 featuresButtons.forEach((x, i) => {
     document.getElementsByClassName("btn_featuredMovie")[i].addEventListener("click", () => {
-        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=a05fba96f4d3bad807d07845d4896afb&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1${x == 0 ? "" : `&with_genres=${x}`}`).then(response => response.json()).then(data => {
-            document.getElementById("featuredList").innerHTML = "";
-            currentFeatured = [];
-            for (let i = 0; i < 12; i++) {
-                currentFeatured.push({
-                    "id" : data.results[i].id,
-                    "poster" : data.results[i].poster_path,
-                    "name" : data.results[i].title,
-                    "year" : data.results[i].release_date.substring(0, 4),
-                    "genre_ids" : data.results[i].genre_ids
-                })
-            };
-            
-            currentFeatured.forEach(x => {
-                genreName(x, x.genre_ids[0]);
-                document.getElementById("featuredList").innerHTML += `<li>${movieCase(x.poster, x.name, x.year, x.genre_name)}</li>`;
-            });
-        });
-    })
+        document.getElementById("featuredList").innerHTML = "";
+        currentFeatured = [];
+        incFilms = 0;
+        currentGenre = x;
+        featuredFilms(x, incFilms);
+        incFilms++;
+        featuredFilms(x, incFilms);
+    });
+})
+
+document.getElementById("more").addEventListener("click", () => {
+    incFilms++;
+    featuredFilms(currentGenre, incFilms);
+    document.getElementById("less").setAttribute("class", "btn");
+})
+
+
+
+document.getElementById("less").addEventListener("click", () => {
+    currentFeatured = currentFeatured.slice(0, 12);
+    incFilms = 1;
+    document.getElementById("featuredList").innerHTML = "";
+    currentFeatured.forEach(x => {
+        genreName(x, x.genre_ids[0]);
+        document.getElementById("featuredList").innerHTML += `<li>${movieCase(x.poster, x.name, x.year, x.genre_name)}</li>`;
+    });
+    document.getElementById("less").setAttribute("class", "btn d-none");
+    document.getElementById("more").setAttribute("class", "btn");
 })
