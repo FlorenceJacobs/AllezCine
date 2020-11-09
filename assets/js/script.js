fc49a79b59c5b8c82f95e8118dcba9c32b88042b
tanoshiibot <fulljelly@outlook.fr>
2020-11-09 11:06:28 +0100

    la modal se remplit avec les informations

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,19 +29,37 @@ window.onload = function() {
         })
     }
 
-
+    let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
     function clickCallback(x) {
         let film = {};
+        let modal = document.getElementById("modalCards");
+        modal.innerHTML = "";
         fetch(`https://api.themoviedb.org/3/movie/${x.id}?api_key=a05fba96f4d3bad807d07845d4896afb&language=en-US`).then(response => response.json()).then(data => {
             film.overview = data.overview;
+            film.title = data.title;
             film.release_date = data.release_date;
+            film.year = data.release_date.substring(0, 4);
+            film.month = months[+data.release_date.substring(5, 7) - 1];
+            film.day = +data.release_date.substring(8, 10);
             film.genres = [];
-            data.genres.forEach(x => film.genres.push(x.id));
+            data.genres.forEach(x => film.genres.push({"id": x.id}));
+            film.genres.forEach(x => genreName(x, x.id));
+            film.genreNames = [];
+            film.genres.forEach(x => film.genreNames.push(x.genre_name));
             film.price = "Free";
         });
         fetch(`https://api.themoviedb.org/3/movie/${x.id}/videos?api_key=a05fba96f4d3bad807d07845d4896afb&language=en-US`).then(response => response.json()).then(data => {
             data.results[0] == undefined ? film.youtube = undefined : film.youtube = data.results[0].id;
         });
+        setTimeout(() => {
+            console.log(film);
+            modal.innerHTML += `<iframe src="https://www.youtube.com/embed/_5f4515dfcee4810037a83d63" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+            modal.innerHTML += `<h4>${film.title}</h4>`;
+            modal.innerHTML += `<div class="d-flex justify-content-between"><h5>Story Line :</h5><p>${film.overview}</p>`;
+            modal.innerHTML += `<div class="d-flex justify-content-between"><h5>Release On :</h5><p>${film.month} ${film.day}, ${film.year}</p>`;
+            modal.innerHTML += `<div class="d-flex justify-content-between"><h5>Genres :</h5><p>${film.genreNames.join(" | ")}</p>`;
+            modal.innerHTML += `<div class="d-flex justify-content-between"><h5>Price :</h5><p>${film.price}</p>`;
+        }, 500)
     }
 
     function addClickOnCards() {
@@ -118,8 +136,8 @@ window.onload = function() {
     incFilms++;
     queryFilm("popularity.desc", 0, incFilms, currentFeatured, "featuredList");
     queryFilm("vote_count.desc", 0, incFilms, currentShop, "shopList");
-    setTimeout(removeClickOnCards, 1000)
-    setTimeout(addClickOnCards, 2000);
+    setTimeout(removeClickOnCards, 500)
+    setTimeout(addClickOnCards, 1000);
 
 
     featuresButtons.forEach((x, i) => {
@@ -131,8 +149,8 @@ window.onload = function() {
             queryFilm("popularity.desc", x, incFilms, currentFeatured, "featuredList");
             incFilms++;
             queryFilm("popularity.desc", x, incFilms, currentFeatured, "featuredList");
-            setTimeout(removeClickOnCards, 1000)
-            setTimeout(addClickOnCards, 2000);
+            setTimeout(removeClickOnCards, 500)
+            setTimeout(addClickOnCards, 1000);
             document.getElementById("less").setAttribute("class", "btn d-none");
         });
     })
@@ -141,8 +159,8 @@ window.onload = function() {
         incFilms++;
         queryFilm("popularity.desc", currentGenre, incFilms, currentFeatured, "featuredList");
         document.getElementById("less").setAttribute("class", "btn");
-        setTimeout(removeClickOnCards, 1000)
-        setTimeout(addClickOnCards, 2000);
+        setTimeout(removeClickOnCards, 500)
+        setTimeout(addClickOnCards, 1000);
     })
 
     document.getElementById("less").addEventListener("click", () => {
@@ -155,20 +173,20 @@ window.onload = function() {
         });
         document.getElementById("less").setAttribute("class", "btn d-none");
         document.getElementById("more").setAttribute("class", "btn");
-        setTimeout(removeClickOnCards, 1000)
-        setTimeout(addClickOnCards, 2000);
+        setTimeout(removeClickOnCards, 500)
+        setTimeout(addClickOnCards, 1000);
     })
 
     //footer
     setTimeout(() => {(currentShop.slice(0,4)).forEach(x => {
             document.getElementById("footerList").innerHTML += `<li class="d-flex align-items-center justify-content-around"><img src="https://image.tmdb.org/t/p/w500${x.img}" alt="No picture available :("><p>${x.name}</p></li>`
         });
-    }, 1000)
+    }, 500)
 
     setTimeout(() => {(currentFeatured.slice(0,6)).forEach(x => {
             document.getElementById("footerTable").innerHTML += `<img src="https://image.tmdb.org/t/p/w500${x.poster}" alt="No picture available :(">`
         });
-    }, 1000);
+    }, 500);
     
 
     //fenêtre des cookies
@@ -202,3 +220,5 @@ window.onload = function() {
     }
 
 };
+
+//<iframe src="https://www.youtube.com/embed/_R1nBwrNf2w" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
